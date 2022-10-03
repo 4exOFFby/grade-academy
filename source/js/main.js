b6ad97987aa4ad74b257053a74e277fc860f5c94
Вася Чехов <4exoff@novoeradio.by>
2022-10-03 09:06:55 +0300

    Сделал секцию приемущества в трёх состояниях

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,9 @@
 import {menuToggler, menuButton} from '../js/main-menu.js';
-import {showNextSlide, showPreviosSlide, sliderButtonNext, sliderButtonPrevios} from '../js/slider.js'
+import {showNextSlide, showPreviousSlide, sliderButtonNext, sliderButtonPrevious} from '../js/slider.js'
 
 
 menuButton.addEventListener('click', menuToggler);
 
 sliderButtonNext.addEventListener('click', showNextSlide);
-sliderButtonPrevios.addEventListener('click', showPreviosSlide);
+sliderButtonPrevious.addEventListener('click', showPreviousSlide);
 
