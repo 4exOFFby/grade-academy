8ccf619aae12b6fb600e978f8b4dd2a8e952cc72
Вася Чехов <4exoff@novoeradio.by>
2022-10-04 16:56:01 +0300

    Добавил работоспособность меню без Js, добавил скрытые заголовки

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,9 @@
-import {menuToggler, menuButton} from '../js/main-menu.js';
+import {menuToggler, menuButton, mainNavigation} from '../js/main-menu.js';
 import {showNextSlide, showPreviousSlide, sliderButtonNext, sliderButtonPrevious} from '../js/slider.js';
 import {mainPinMarker, map} from '../js/map.js';
 import {sortingMenuButton, openSortingMenuHandler} from '../js/sorting-menu.js';
+
+mainNavigation.classList.remove('main-header__navigation--no-js');
 
 menuButton.addEventListener('click', menuToggler);
 
